10838ac6d701d22f70c62370c0f5f6c03d8bd7f0
caitlynmayers <caitlynmcarney@gmail.com>
2014-12-20 18:28:20 -0500

    adding proper files and getting JS to randomize data insertion into table cells.

---
+++ js/site.js
@@ -0,0 +1,46 @@
+// onLoad
+
+$( document ).ready(function() {
+  var random = randomize();
+  displayContents(random);
+});
+
+function randomNumber(maxNum){
+	return Math.floor(Math.random() * maxNum);
+}
+
+function randomize(){
+	var contents = ['Army', 'Combat Engineer', 'BAM!', 'Pull a Hank Snow', 'Were you in the war?', 'Chop his head off', 'Have gun will travel', 'Where\'s my bride?', 'Green green grass of home', 'I had two words for him', 'Did you graduate?', 'Sons of bitches', 'They taught us to kill', 'Kilroy was here', 'She changed my vocabulary', 'I never did drink', 'Ladies', 'When I came back', 'Go to hell', 'New Guinea, Biac, Leite, Louzon and Japan', 'Break his neck', 'Carry a gun', 'Car', 'Shoot', 'I could kill you' ];
+	var randomContents = [];
+
+	while(randomContents.length != contents.length){
+		var randomIndex = randomNumber(contents.length);
+		var item = contents[randomIndex];
+		var itemIndex = randomContents.indexOf(item);
+		
+		if(itemIndex == -1){
+			randomContents[randomContents.length] = item;
+		}
+	}
+	
+	return randomContents;
+	
+}
+
+function displayContents(randomContents){
+	var rows = document.getElementById('bingo').rows;
+	var contentsIndex = 0;
+	for(var rowIndex = 1; rowIndex < rows.length; rowIndex++){
+		var row = rows[rowIndex];
+		
+		for(var cellIndex = 0; cellIndex < row.cells.length; cellIndex++){
+			if(contentsIndex < randomContents.length){
+				var text = randomContents[contentsIndex];
+				var cell = row.cells[cellIndex];
+				cell.innerHTML = text;
+			}
+			contentsIndex++;
+		}
+	}
+
+}
